4114631c06829c478e8555c553d64ebe8203ee62
Gabrielpfreitas <56432521+Gabrielpfreitas@users.noreply.github.com>
2022-07-27 12:19:06 +0200

    Update juniperberry.py

--- juniperberry.py
+++ juniperberry.py
@@ -1,8 +1,3 @@
-#__________________________________________________________________________________________________________________________________________#
-# MULTIPARAMETER BIOAEROSOL SPECTROMETER PYTHON FUNCTIONS
-# author: Gabriel Pereira Freitas
-# contact: gabriel.freitas@aces.su.se
-
 import numpy as np
 import pandas as pd
 import glob2
@@ -18,6 +13,17 @@ def printc(s=None,os_n = 1, skip=False):
     if skip == False:
     
         os.write(os_n, (s+'\n').encode())
+        
+def qfig(x=10,y=5,x1=1,x2=1,n=1):
+    
+    fig = plt.figure(figsize=(x,y))
+    
+    pl  = plt.subplot(x1,x2,n)
+    
+    return fig, pl
+    
+    
+    
         
         
         
