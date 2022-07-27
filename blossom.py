8c9ef4b0fbdaf9c3087c4a1c024b9131f2b5e1a5
Gabrielpfreitas <56432521+Gabrielpfreitas@users.noreply.github.com>
2022-07-27 12:06:25 +0200

    Rename auxberry to acaiberry

--- blossom.py
+++ blossom.py
@@ -1,22 +1,22 @@
-import sys
 import os
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-import mulberry as mb
 import pandas as pd
 import numpy as np
 import glob2
+import tqdm
+
+##### collect berries
+import acaiberry as ab
 import cloudberry as cb
 import juniperberry as jb
-import auxberry as ab
-import tqdm
-from IPython.display import clear_output
-jb.printc('Packages initiliazed')
+import mulberry as mb
 
+
+
+#Useful functions
+from IPython.display import clear_output
 #Open console
 # %qtconsole
-
-jb.printc('Console opened')
 # mpl.rc('font',family='consolas')
 mpl.rc('font',size=14)
-jb.printc('Matplotlib common commands given')
