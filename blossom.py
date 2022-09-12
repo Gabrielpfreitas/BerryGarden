17d22b325e09899db4183f9210dfb512cd3933c3
Gabrielpfreitas <56432521+Gabrielpfreitas@users.noreply.github.com>
2022-09-12 11:16:18 +0200

    Update blossom.py

--- blossom.py
+++ blossom.py
@@ -5,9 +5,11 @@ import matplotlib.patches as patches
 import matplotlib.patheffects as pe
 import pandas as pd
 import numpy as np
-import glob2
+# import glob2
+import glob
 import tqdm
 import datetime as dt
+
 
 ##### collect berries
 import acaiberry as ab
@@ -16,10 +18,9 @@ import juniperberry as jb
 import mulberry as mb
 
 
-
 #Useful functions
 from IPython.display import clear_output
 #Open console
 # %qtconsole
 # mpl.rc('font',family='consolas')
-mpl.rc('font',size=14)
+# mpl.rc('font',size=14)
